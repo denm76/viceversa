4dfc7ef07b9f80f3d75df01649514588d614dca6
denm76 <denm76@gmail.com>
2021-04-02 11:37:56 +0300

    To page added count feature in text

--- viceversa/views.py
+++ viceversa/views.py
@@ -8,5 +8,6 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    count_of_words = len(user_text.split())
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'words_count': count_of_words})
